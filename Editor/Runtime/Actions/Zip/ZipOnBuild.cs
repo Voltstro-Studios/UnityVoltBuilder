27d8ce56c5361c97ea2d13a11d7b4f6d2ee4211d
Voltstro <me@voltstro.dev>
2020-12-25 01:29:23 +1000

    Added zip build action

---
+++ Editor/Runtime/Actions/Zip/ZipOnBuild.cs
@@ -0,0 +1,58 @@
+﻿#if ZIP //We can only do this if the com.unity.sharp-zip-lib package is installed
+
+using System.Diagnostics;
+using System.IO;
+using Unity.SharpZipLib.Utils;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using Voltstro.UnityBuilder.Actions;
+using Voltstro.UnityBuilder.Settings;
+using Debug = UnityEngine.Debug;
+
+public class ZipOnBuild : IBuildAction
+{
+	public void OnGUI()
+	{
+		ZipBuild = EditorGUILayout.Toggle("Zip Build", ZipBuild);
+	}
+
+	public void OnBeforeBuild(string buildLocation)
+	{
+		
+	}
+
+	public void OnAfterBuild(string buildLocation, BuildReport report)
+	{
+		if (ZipBuild)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			string outPath = $"{buildLocation}/../{Path.GetFileName(buildLocation)}.zip";
+			Debug.Log("Compressing build...");
+			ZipUtility.CompressFolderToZip(outPath, null, buildLocation);
+
+			stopwatch.Stop();
+			Debug.Log($"Compressed build to {outPath}. Took {stopwatch.Elapsed.Seconds}s to compress.");
+		}
+	}
+
+	private static bool ZipBuild
+	{
+		get
+		{
+			if (!SettingsManager.Instance.ContainsKey<bool>("ZipBuild"))
+			{
+				SettingsManager.Instance.Set("ZipBuild", true);
+				SettingsManager.Instance.Save();
+			}
+
+			return SettingsManager.Instance.Get<bool>("ZipBuild");
+		}
+		set
+		{
+			SettingsManager.Instance.Set("ZipBuild", value);
+			SettingsManager.Instance.Save();
+		}
+	}
+}
+
+#endif
