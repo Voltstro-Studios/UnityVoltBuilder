f756e9353285191955d89ca30d5c9401a755ffbe
Voltstro <me@voltstro.dev>
2021-06-01 21:43:33 +1000

    Improved settings

--- Editor/Runtime/Settings/SettingsManager.cs
+++ Editor/Runtime/Settings/SettingsManager.cs
@@ -3,6 +3,12 @@ using UnityEditor;
 
 namespace Voltstro.UnityBuilder.Settings
 {
+	/// <summary>
+	///		Manages settings for VoltUnityBuilder
+	///		<para>
+	///			Feel free to use this to store your settings for your custom build actions
+	///		</para>
+	/// </summary>
 	public static class SettingsManager
 	{
 		private const string PackageName = "dev.voltstro.voltunitybuilder";
@@ -10,208 +16,117 @@ namespace Voltstro.UnityBuilder.Settings
 		private static UnityEditor.SettingsManagement.Settings settings;
 
 		/// <summary>
-		/// The active <see cref="Settings"/> instance
+		///		The active <see cref="Settings"/> instance
 		/// </summary>
-		public static UnityEditor.SettingsManagement.Settings Instance
-		{
-			get
-			{
-				if (settings == null)
-					settings = new UnityEditor.SettingsManagement.Settings(PackageName);
+		public static UnityEditor.SettingsManagement.Settings Instance => settings ??= new UnityEditor.SettingsManagement.Settings(PackageName);
 
-				return settings;
-			}
+		///  <summary>
+		/// 		Add or gets an option from <see cref="Instance"/>
+		///  </summary>
+		///  <typeparam name="T"></typeparam>
+		///  <param name="key"></param>
+		///  <param name="defaultValue"></param>
+		///  <returns></returns>
+		public static T AddOrGetOption<T>(string key, T defaultValue = default(T))
+		{
+			UnityEditor.SettingsManagement.Settings settingsInstance = Instance;
+			if (!settingsInstance.ContainsKey<T>(key))
+				SetOption<T>(key, defaultValue);
+
+			return settingsInstance.Get<T>(key);
+		}
+
+		/// <summary>
+		///		Sets an option from <see cref="Instance"/>
+		/// </summary>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
+		/// <typeparam name="T"></typeparam>
+		public static void SetOption<T>(string key, T value)
+		{
+			UnityEditor.SettingsManagement.Settings settingsInstance = Instance;
+			settingsInstance.Set(key, value);
+			settingsInstance.Save();
 		}
 
 		#region Properties
 
 		#region Build Path Settings
 
-		internal static string BuildLocation
+		private const string BuildLocationKey = "BuildLocation";
+		public static string BuildLocation
 		{
-			get
-			{
-				if (Instance.ContainsKey<string>("BuildLocation")) return Instance.Get<string>("BuildLocation");
-
-				Instance.Set("BuildLocation", "Builds/");
-				Instance.Save();
-
-				return Instance.Get<string>("BuildLocation");
-			}
-			set
-			{
-				Instance.Set("BuildLocation", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption(BuildLocationKey, "Builds/");
+			internal set => SetOption(BuildLocationKey, value);
 		}
 
-		internal static string BuildFolderNameStyle
+		private const string BuildFolderNameStyleKey = "BuildFolderNameStyle";
+		public static string BuildFolderNameStyle
 		{
-			get
-			{
-				if (Instance.ContainsKey<string>("BuildFolderNameStyle")) return Instance.Get<string>("BuildFolderNameStyle");
-
-				Instance.Set("BuildFolderNameStyle", "yy-MM-dd");
-				Instance.Save();
-
-				return Instance.Get<string>("BuildFolderNameStyle");
-			}
-			set
-			{
-				Instance.Set("BuildFolderNameStyle", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption(BuildFolderNameStyleKey, "yy-MM-dd");
+			internal set => SetOption(BuildFolderNameStyleKey, value);
 		}
 
 		#endregion
 
 		#region Build Player Settings
 
-		internal static BuildTarget BuildTarget
+		private const string BuildTargetKey = "BuildTarget";
+		public static BuildTarget BuildTarget
 		{
-			get
-			{
-				if (Instance.ContainsKey<BuildTarget>("BuildTarget")) return Instance.Get<BuildTarget>("BuildTarget");
-
-				Instance.Set("BuildTarget", BuildTarget.StandaloneWindows64);
-				Instance.Save();
-
-				return Instance.Get<BuildTarget>("BuildTarget");
-			}
-			set
-			{
-				Instance.Set("BuildTarget", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption(BuildTargetKey, EditorUserBuildSettings.activeBuildTarget);
+			internal set => SetOption(BuildTargetKey, value);
 		}
 
-		internal static bool ServerBuild
+		private const string ServerBuildKey = "ServerBuild";
+		public static bool ServerBuild
 		{
-			get
-			{
-				if (Instance.ContainsKey<bool>("ServerBuild")) return Instance.Get<bool>("ServerBuild");
-
-				Instance.Set("ServerBuild", false);
-				Instance.Save();
-
-				return Instance.Get<bool>("ServerBuild");
-			}
-			set
-			{
-				Instance.Set("ServerBuild", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption<bool>(ServerBuildKey);
+			internal set => SetOption(ServerBuildKey, value);
 		}
 
-		internal static bool CopyPdbFiles
+		private const string CopyPdbFilesKey = "CopyPDBFiles";
+		public static bool CopyPdbFiles
 		{
-			get
-			{
-				if (Instance.ContainsKey<bool>("CopyPDBFiles")) return Instance.Get<bool>("CopyPDBFiles");
-
-				Instance.Set("CopyPDBFiles", false);
-				Instance.Save();
-
-				return Instance.Get<bool>("CopyPDBFiles");
-			}
-			set
-			{
-				Instance.Set("CopyPDBFiles", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption<bool>(CopyPdbFilesKey);
+			internal set => SetOption(CopyPdbFilesKey, value);
 		}
 
-		internal static bool DevelopmentBuild
+		private const string DevelopmentBuildKey = "DevelopmentBuild";
+		public static bool DevelopmentBuild
 		{
-			get
-			{
-				if (Instance.ContainsKey<bool>("DevelopmentBuild")) return Instance.Get<bool>("DevelopmentBuild");
-
-				Instance.Set("DevelopmentBuild", false);
-				Instance.Save();
-
-				return Instance.Get<bool>("DevelopmentBuild");
-			}
-			set
-			{
-				Instance.Set("DevelopmentBuild", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption<bool>(DevelopmentBuildKey);
+			internal set => SetOption(DevelopmentBuildKey, value);
 		}
 
-		internal static bool AutoconnectProfiler
+		private const string AutoconnectProfilerKey = "AutoconnectProfiler";
+		public static bool AutoconnectProfiler
 		{
-			get
-			{
-				if (Instance.ContainsKey<bool>("AutoconnectProfiler")) return Instance.Get<bool>("AutoconnectProfiler");
-
-				Instance.Set("AutoconnectProfiler", false);
-				Instance.Save();
-
-				return Instance.Get<bool>("AutoconnectProfiler");
-			}
-			set
-			{
-				Instance.Set("AutoconnectProfiler", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption<bool>(AutoconnectProfilerKey);
+			internal set => SetOption(AutoconnectProfilerKey, value);
 		}
 
-		internal static bool DeepProfiling
+		private const string DeepProfilingKey = "DeepProfiling";
+		public static bool DeepProfiling
 		{
-			get
-			{
-				if (Instance.ContainsKey<bool>("DeepProfiling")) return Instance.Get<bool>("DeepProfiling");
-
-				Instance.Set("DeepProfiling", false);
-				Instance.Save();
-
-				return Instance.Get<bool>("DeepProfiling");
-			}
-			set
-			{
-				Instance.Set("DeepProfiling", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption<bool>(DeepProfilingKey);
+			internal set => SetOption(DeepProfilingKey, value);
 		}
 
-		internal static bool ScriptDebugging
+		private const string ScriptDebuggingKey = "ScriptDebugging";
+		public static bool ScriptDebugging
 		{
-			get
-			{
-				if (Instance.ContainsKey<bool>("ScriptDebugging")) return Instance.Get<bool>("ScriptDebugging");
-
-				Instance.Set("ScriptDebugging", false);
-				Instance.Save();
-
-				return Instance.Get<bool>("ScriptDebugging");
-			}
-			set
-			{
-				Instance.Set("ScriptDebugging", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption<bool>(ScriptDebuggingKey);
+			internal set => SetOption(ScriptDebuggingKey, value);
 		}
 
 		#endregion
 
-		internal static List<string> BuildActions
+		private const string BuildActionsKey = "BuildActions";
+		public static List<string> BuildActions
 		{
-			get
-			{
-				if (!Instance.ContainsKey<List<string>>("BuildActions"))
-				{
-					Instance.Set("BuildActions", new List<string>());
-					Instance.Save();
-				}
-
-				return Instance.Get<List<string>>("BuildActions");
-			}
-			set
-			{
-				Instance.Set("BuildActions", value);
-				Instance.Save();
-			}
+			get => AddOrGetOption(BuildActionsKey, new List<string>());
+			internal set => SetOption(BuildActionsKey, value);
 		}
 
 		#endregion
