b5aee0cc15bf1a037043c4c886deb6358dc7364b
Voltstro <me@voltstro.dev>
2021-10-17 23:17:27 +1000

    Update window draw options

--- Editor/Runtime/GUI/BuildToolWindow.cs
+++ Editor/Runtime/GUI/BuildToolWindow.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
 using UnityVoltBuilder.Actions;
 using UnityVoltBuilder.Build;
@@ -7,6 +10,13 @@ namespace UnityVoltBuilder.GUI
 {
     public sealed class BuildToolWindow : EditorWindow
     {
+        private readonly List<Action> windowDraws = new List<Action>
+        {
+            BuildSettings.DrawOptions,
+            BuildActions.DrawOptions,
+            GameBuilder.DrawOptions
+        };
+
         public void OnGUI()
         {
             //Main title
@@ -14,9 +24,31 @@ namespace UnityVoltBuilder.GUI
             EditorGUILayout.LabelField("Unity Volt Builder", GUIStyles.TitleStyle);
             EditorGUILayout.Space(15f);
 
-            BuildSettings.DrawOptions();
-            BuildActions.DrawOptions();
-            GameBuilder.DrawOptions();
+            foreach (Action windowDraw in windowDraws)
+            {
+                try
+                {
+                    windowDraw.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Error occured while drawing options! {ex}");
+                }
+            }
+        }
+
+        [PublicAPI]
+        public void AddOnDraw([NotNull] Action drawAction)
+        {
+            if (drawAction == null)
+                throw new ArgumentNullException(nameof(drawAction), "Draw action cannot be null!");
+
+            if (windowDraws.Contains(drawAction))
+            {
+                Debug.LogError("Draw action has already been added!");
+                return;
+            }
+            windowDraws.Add(drawAction);
         }
 
         [MenuItem("Tools/Volt Unity Builder/Volt Builder")]
