8b939c7f0e6e228e5bca2d961846ae75b47b022e
Voltstro <me@voltstro.dev>
2021-10-17 22:23:54 +1000

    Update zip build action

---
+++ Editor/Runtime/Actions/ZipOnBuild.cs
@@ -0,0 +1,51 @@
+﻿#if ZIPPING_SUPPORT //We can only do this if the com.unity.sharp-zip-lib package is installed
+
+using System.Diagnostics;
+using System.IO;
+using Unity.SharpZipLib.Utils;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityVoltBuilder.Settings;
+using Debug = UnityEngine.Debug;
+
+namespace UnityVoltBuilder.Actions
+{
+	public class ZipOnBuild : IBuildAction
+	{
+		public void OnGUI()
+		{
+			ZipBuild = EditorGUILayout.Toggle("Zip Build", ZipBuild);
+		}
+
+		public void OnBeforeBuild(string buildLocation, BuildTarget buildTarget, ref BuildOptions buildOptions)
+		{
+		}
+
+		public void OnAfterBuild(string buildLocation, BuildReport report)
+		{
+			if (!ZipBuild) 
+				return;
+			
+			Debug.Log("Compressing build...");
+			
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			
+			string outPath = $"{buildLocation}/../{Path.GetFileName(buildLocation)}.zip";
+			ZipUtility.CompressFolderToZip(outPath, null, buildLocation);
+
+			stopwatch.Stop();
+			
+			Debug.Log($"Compressed build to {outPath}. Took {stopwatch.Elapsed.Seconds}s to compress.");
+		}
+
+		private const string ZipBuildKey = "ZipBuild";
+
+		public static bool ZipBuild
+		{
+			get => SettingsManager.AddOrGetOption(ZipBuildKey, true);
+			set => SettingsManager.SetOption(ZipBuildKey, value);
+		}
+	}
+}
+
+#endif
